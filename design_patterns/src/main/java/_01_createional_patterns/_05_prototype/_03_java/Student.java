1fdd2d3db6b5c1e2d9ea8047b91482b593952cb0
mylog6190 <adg6190@naver.com>
2023-08-23 23:13:33 +0900

    commit

---
+++ design_patterns/src/main/java/_01_createional_patterns/_05_prototype/_03_java/Student.java
@@ -0,0 +1,17 @@
+package _01_createional_patterns._05_prototype._03_java;
+
+public class Student {
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
