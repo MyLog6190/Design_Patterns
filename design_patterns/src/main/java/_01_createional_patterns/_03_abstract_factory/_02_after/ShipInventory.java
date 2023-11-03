38830259e660fe1236a3bdf0a9a7ecae699c4dce
mylog6190 <adg6190@naver.com>
2023-11-03 23:02:03 +0900

    commit

--- design_patterns/src/main/java/_01_createional_patterns/_03_abstract_factory/_02_after/ShipInventory.java
+++ design_patterns/src/main/java/_01_createional_patterns/_03_abstract_factory/_02_after/ShipInventory.java
@@ -6,7 +6,8 @@ import _01_createional_patterns._03_abstract_factory._01_before.ShipFactory;
 public class ShipInventory {
     /*
     * Abstract Factory Pattern
-    *
+    * - 연관성 있는 객체들의 묶고 각자의 인터페이스를 생성하고 객체들을 각자 팩토리 패턴을 이용해 만들어 사용하는 패턴이다.
+    * - 팩토리 패턴을 사용한 객체들을 묶어 함께 사용하는 패턴이다.
     *
     * Factory Method와 Abstract Factory Method 차이
     *
@@ -19,7 +20,7 @@ public class ShipInventory {
     * - 여러 객체들의 생성을 캡슐화하여 함께사용한다.
     * - 추상 팩토리 패턴으로 만든 객체를 조합하여 팩토리 매소드를 만들어 사용한다.
     *
-    *
+    * Abstract Factory Pattern
     *
     * */
 
